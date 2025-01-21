989d689bebe37db2c923d32626e495e6097163f9
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-20 19:38:29 -0700

    add index, show, and create methods to controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,25 @@
 class SongsController < ApplicationController
+  
+  def index
+    @songs = Song.all
+    render :index
+
+  end
+
+  def show
+    id_find = params[:id]
+    @song = Song.find_by(id: id_find)
+    render :show
+  end
+
+  def create
+    @song = Song.new(
+      title: params[:input_title],
+      album: params[:input_album],
+      artist: params[:input_artist],
+      year: params[:input_year]
+    )
+    @song.save
+    render :show
+  end
 end
